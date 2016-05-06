b6ee52e2e3165c766e8c515c35c83bbfc612c56c
Vnox <vnoxdev@gmail.com>
2016-05-05 20:09:32 -0700

    View changed, deprecated problem introduced ) :

--- app/src/main/java/com/android/vnoxdev/coupletunes/LoginScreen.java
+++ app/src/main/java/com/android/vnoxdev/coupletunes/LoginScreen.java
@@ -1,13 +1,66 @@
 package com.android.vnoxdev.coupletunes;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginScreen extends AppCompatActivity {
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnRegId;
+    EditText etRegId;
+    GoogleCloudMessaging gcm;
+    String regid;
+    String PROJECT_NUMBER = "813916479990";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        btnRegId = (Button) findViewById(R.id.GIDButton);
+        etRegId = (EditText) findViewById(R.id.userIdTag);
+
+        btnRegId.setOnClickListener(this);
+    }
+
+    public void getRegId() {
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if(gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+                    }
+
+                    regid = gcm.register(PROJECT_NUMBER);
+                    msg = "Device registered, registration ID=" + regid;
+                    Log.i("GCM", "!!!!! " + regid);
+
+                } catch(IOException ex) {
+                    msg = "Error: " + ex.getMessage();
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                etRegId.setText(msg);
+            }
+        }.execute(null, null, null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        getRegId();
     }
 }
